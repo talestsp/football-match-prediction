e9ed2df6fd8c7ff3fe1d738113a438965c3f5285
tales.pimentel.ter <2x6ewwbkugotusncutbvo4vu52vs2degnyuytyquqetow6psvira>
2022-04-24 21:07:36 -0300

    Adding round_cols functions

--- src/utils/dflib.py
+++ src/utils/dflib.py
@@ -1,2 +1,9 @@
+import pyspark.sql.functions as f
+
 def shape(df):
     return df.count(), len(df.columns)
+
+def round_cols(df, colnames, round_n=2):
+    for colname in colnames:
+        df = df.withColumn(colname, f.round(colname, round_n))
+    return df
