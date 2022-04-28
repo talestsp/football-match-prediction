ab94d402c7a3f74dab093e1580c0058ae0321d04
tales.pimentel.ter <2x6ewwbkugotusncutbvo4vu52vs2degnyuytyquqetow6psvira>
2022-04-27 21:09:25 -0300

    Defining color palettes

--- src/utils/palette.py
+++ src/utils/palette.py
@@ -1,4 +1,7 @@
 import seaborn as sns
 
 PALETTE = sns.color_palette("tab10").as_hex()
-DATASETS_PAL = {'train': PALETTE[0], 'test': PALETTE[1], 'validation': PALETTE[2]}
+PALETTE_DATASETS = {'train': PALETTE[0], 'test': PALETTE[1], 'validation': PALETTE[2]}
+PALETTE_TARGET = {'home': PALETTE[2], 'draw': PALETTE[1], 'away': PALETTE[4]}
+
+
