ea201320b7fa21fd2070189b22b8f17fe19fa7c4
tales.pimentel.ter <2x6ewwbkugotusncutbvo4vu52vs2degnyuytyquqetow6psvira>
2022-05-01 19:45:36 -0300

    grouping color palettes by mood and result

--- src/utils/palette.py
+++ src/utils/palette.py
@@ -3,5 +3,6 @@ import seaborn as sns
 PALETTE = sns.color_palette("tab10").as_hex()
 PALETTE_DATASETS = {'train': PALETTE[0], 'test': PALETTE[1], 'validation': PALETTE[2]}
 PALETTE_TARGET = {'home': PALETTE[2], 'draw': PALETTE[1], 'away': PALETTE[4]}
+PALETTE_MOOD = {"pessimistic": '#d88282', 'neutral': "#f0ddbf", 'optimistic': "#83b5bd"}
 
 
