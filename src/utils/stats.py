78fc3ef48983f68405bd5ca7924cc2a4441610aa
tales.pimentel.ter <2x6ewwbkugotusncutbvo4vu52vs2degnyuytyquqetow6psvira>
2022-05-02 19:41:08 -0300

    Confidence interval functions

--- src/utils/stats.py
+++ src/utils/stats.py
@@ -1,3 +1,6 @@
+import numpy as np
+import scipy.stats
+import operator
 import pyspark.sql.functions as f
 from pyspark.sql.window import Window
 from src.utils import dflib
@@ -82,3 +85,51 @@ def describe_cols(df, colnames, round_n=None):
         describe_cols_df = describe_cols_df.union(describe(df, colname, round_n=round_n))
 
     return describe_cols_df
+
+def mean_confidence_interval(data_list, confidence=0.95):
+    a = 1.0 * np.array(data_list)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+    return m, m-h, m+h
+
+def non_param_paired_ci_median(sample1, sample2, conf=0.95):
+    #https://towardsdatascience.com/prepare-dinner-save-the-day-by-calculating-confidence-interval-of-non-parametric-statistical-29d031d079d0
+
+    # custom func for confidence interval for differences
+    # for (non-Gaussian paired data) Example: Wilcoxon signed-rank test
+    n = len(sample1)
+    alpha = 1-conf
+    N = scipy.stats.norm.ppf(1 - alpha/2)
+
+    # The confidence interval for the difference between the two population
+    # medians is derived through the n(n+1)/2 possible averaged differences.
+    diff_sample = sorted(list(map(operator.sub, sample2, sample1)))
+    averages = sorted([(s1+s2)/2 for i, s1 in enumerate(diff_sample) for _, s2 in enumerate(diff_sample[i:])])
+
+    # the Kth smallest to the Kth largest of the averaged differences then
+    # determine the confidence interval, where K is:
+    k = np.math.ceil(n*(n+1)/4 - (N * (n*(n+1)*(2*n+1)/24)**0.5))
+
+    ci = (round(averages[k-1],3), round(averages[len(averages)-k],3))
+    return ci
+
+def non_param_unpaired_ci_median(sample1, sample2, conf=0.95):
+    #https://towardsdatascience.com/prepare-dinner-save-the-day-by-calculating-confidence-interval-of-non-parametric-statistical-29d031d079d0
+    # custom func for confidence interval for differences
+    # for (non-Gaussian unpaired data). Example: Mann-Whitney U test
+    n1 = len(sample1)
+    n2 = len(sample2)
+    alpha = 1 - conf
+    N = scipy.stats.norm.ppf(1 - alpha / 2)
+
+    # The confidence interval for the difference between the two population
+    # medians is derived through the n x m differences.
+    diffs = sorted([i - j for i in sample1 for j in sample2])
+
+    # the Kth smallest to the Kth largest of the n x m differences then determine
+    # the confidence interval, where K is:
+    k = np.math.ceil(n1 * n2 / 2 - (N * (n1 * n2 * (n1 + n2 + 1) / 12) ** 0.5))
+
+    ci = (round(diffs[k - 1], 3), round(diffs[len(diffs) - k], 3))
+    return ci
