c68eb01839818cb5db779221cce4a3281c15f125
tales.pimentel.ter <2x6ewwbkugotusncutbvo4vu52vs2degnyuytyquqetow6psvira>
2022-05-09 11:53:12 -0300

    Adding functions for generating home_factor features for whole train dataset (train_train and train_valid combined)

--- src/dao/dao_interim.py
+++ src/dao/dao_interim.py
@@ -3,6 +3,7 @@ from src.dao import dao, dao_raw
 INTERIM_DATA_DIR = dao.DATA_DIR + "interim/"
 FILEPATH_TRAIN_TRAIN_IDS = INTERIM_DATA_DIR + "train_train_ids.csv"
 FILEPATH_TRAIN_VALID_IDS = INTERIM_DATA_DIR + "train_valid_ids.csv"
+FILEPATH_FT_HOME_FACTOR_WHOLE_TRAIN = INTERIM_DATA_DIR + "home_factor_whole_train.csv"
 SEP = ";"
 
 def load_train_train_ids(spark, header=True):
@@ -12,10 +13,10 @@ def load_train_valid_ids(spark, header=True):
     return dao.load_parse_data(FILEPATH_TRAIN_VALID_IDS, spark, header=header, sep=SEP)
 
 def save_train_train_ids(df):
-    return dao.save_data(df, FILEPATH_TRAIN_TRAIN_IDS, sep=SEP)
+    dao.save_data(df, FILEPATH_TRAIN_TRAIN_IDS, sep=SEP)
 
 def save_train_valid_ids(df):
-    return dao.save_data(df, FILEPATH_TRAIN_VALID_IDS, sep=SEP)
+    dao.save_data(df, FILEPATH_TRAIN_VALID_IDS, sep=SEP)
 
 def load_train_train_data(spark, header=True):
     train_train_ids = dao.load_parse_data(FILEPATH_TRAIN_TRAIN_IDS, spark, header=header, sep=SEP)
@@ -26,3 +27,12 @@ def load_train_valid_data(spark, header=True):
     train_valid_ids = dao.load_parse_data(FILEPATH_TRAIN_VALID_IDS, spark, header=header, sep=SEP)
     df = dao_raw.load_parse_train_data(spark).join(train_valid_ids, on="id", how="leftsemi")
     return df
+
+def load_home_factor_whole_train(spark, header=True):
+    df = dao.load_data(filepath=FILEPATH_FT_HOME_FACTOR_WHOLE_TRAIN,
+                       spark=spark, header=header, sep=SEP)
+    return df
+
+def save_home_factor_whole_train(df):
+    dao.save_data(df.select(["league_id", "home_factor", "draw_factor", "n_matches"]),
+                  FILEPATH_FT_HOME_FACTOR_WHOLE_TRAIN, sep=SEP)
